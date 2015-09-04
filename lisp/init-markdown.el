d74d946ad00d41ad7e8a68be7a51099933db3f25
cadl <ctrlaltdeleteliu@gmail.com>
2015-09-05 00:09:36 +0800

    set markdown-command to pandoc

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -2,5 +2,6 @@
 
 (setq auto-mode-alist
       (cons '("\\.\\(md\\|markdown\\)\\'" . markdown-mode) auto-mode-alist))
-
+(custom-set-variables
+ '(markdown-command "/usr/bin/pandoc"))
 (provide 'init-markdown)
