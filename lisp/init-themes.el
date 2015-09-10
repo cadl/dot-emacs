0efb86f6f328bad1a21e3acf4f2af3a13ce6e211
cadl <ctrlaltdeleteliu@gmail.com>
2015-09-10 16:21:10 +0800

    make default theme dark

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,6 +1,6 @@
 (require-package 'color-theme-solarized)
 
-;;; (set-frame-parameter nil 'background-mode 'dark)
+(set-frame-parameter nil 'background-mode 'dark)
 (load-theme 'solarized t)
 
 (provide 'init-themes)
