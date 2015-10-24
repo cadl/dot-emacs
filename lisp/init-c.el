dea180afbc3c23687f0ceab956bf820691bd91b3
cadl <ctrlaltdeleteliu@gmail.com>
2015-10-24 20:39:14 +0800

    Use company-irony to complete c code

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,4 +1,5 @@
 (require-package 'irony)
+(require-package 'company-irony)
 
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
@@ -6,12 +7,19 @@
 
 ;; replace the `completion-at-point' and `complete-symbol' bindings in
 ;; irony-mode's buffers by irony-mode's function
-(defun my-irony-mode-hook ()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;;(defun my-irony-mode-hook ()
+;;  (define-key irony-mode-map [remap completion-at-point]
+;;    'irony-completion-at-point-async)
+;;  (define-key irony-mode-map [remap complete-symbol]
+;;    'irony-completion-at-point-async))
+;;(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+(defun company-irony-hook()
+  (company-mode 1))
+
+(add-hook 'irony-mode-hook 'company-irony-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 (provide 'init-c)
