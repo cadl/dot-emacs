510bb166542008bb0c41b6ed83fbcaeab52b86df
cadl <ctrlaltdeleteliu@gmail.com>
2015-10-24 20:52:41 +0800

    enable semantic-mode

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -5,21 +5,11 @@
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
 
-;; replace the `completion-at-point' and `complete-symbol' bindings in
-;; irony-mode's buffers by irony-mode's function
-;;(defun my-irony-mode-hook ()
-;;  (define-key irony-mode-map [remap completion-at-point]
-;;    'irony-completion-at-point-async)
-;;  (define-key irony-mode-map [remap complete-symbol]
-;;    'irony-completion-at-point-async))
-;;(add-hook 'irony-mode-hook 'my-irony-mode-hook)
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-irony))
 
-(defun company-irony-hook()
-  (company-mode 1))
-
-(add-hook 'irony-mode-hook 'company-irony-hook)
+(add-hook 'irony-mode-hook 'company-irony)
+(add-hook 'irony-mode-hook 'semantic-mode)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 (provide 'init-c)
