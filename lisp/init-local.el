b9c266acc2e7c6925c2abfe6f5df98f30e4e030f
cadl <ctrlaltdeleteliu@gmail.com>
2015-11-07 20:27:20 +0800

    set mac font

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,7 @@
 (blink-cursor-mode 0)
 (desktop-save-mode 0)
-(set-default-font "Monaco 14")
+(if (eq system-type 'darwin)
+    (set-default-font "Monaco 14"))
 (set-fontset-font "fontset-default" 'han '("Ping Fang"))
 (setq ring-bell-function #'ignore)
 (provide 'init-local)
