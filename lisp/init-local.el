1defe94dcfd694c178f38db2d1ce5559ad0f7bba
cadl <ctrlaltdeleteliu@gmail.com>
2016-01-09 22:06:24 +0800

    exec path from shell

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,7 +1,12 @@
+(require-package 'exec-path-from-shell)
+
 (blink-cursor-mode 0)
 (desktop-save-mode 0)
 (if (eq system-type 'darwin)
     (set-default-font "Monaco 14"))
 (set-fontset-font "fontset-default" 'han '("Ping Fang"))
 (setq ring-bell-function #'ignore)
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 (provide 'init-local)
